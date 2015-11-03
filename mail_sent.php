6c4f3134574945a18b9feeb1143968ccd7d13680
gautam188 <gautam_188@yahoo.com>
2015-11-03 16:03:04 +0530

    Gautam

--- mail_sent.php
+++
@@ -1,8 +0,0 @@
-<?php        
-        $to = 'technologygroups09@gmail.com'; 
-        require 'mailer/Send_Mail.php';
-        $subject = "Test Mail";
-
-        $body = "Test of mail sending";
-        Send_Mail($to,$subject,$body);                    
-?>
--- random.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function mt_rand_str ($l, $c = 'ABCDEFGHIJKLMNOPQRSTYVWXYZabcdefghijklmnopqrstuvwxyz1234567890') {
-    for ($s = '', $cl = strlen($c)-1, $i = 0; $i < $l; $s .= $c[mt_rand(0, $cl)], ++$i);
-    return $s;
-}
-
-//echo mt_rand_str(8); // Something like mp2tmpsw
-echo mt_rand_str(10); // Something like B9CD0F
