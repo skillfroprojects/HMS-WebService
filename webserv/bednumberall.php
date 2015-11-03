e4d28bbf724898d30ec60fafb7d26d5479e33853
reema.desai <reema.desai@SKILLFRO-ADMIN>
2015-11-03 16:49:34 +0530

    Reema-One

--- webserv/bednumberall.php
+++ webserv/bednumberall.php
@@ -2,26 +2,21 @@
 include 'include/Config.php';
 $db = new DB_Class();
 
-
-if (isset($_POST['br_id'])) {
-
-    $BR_ID = $_POST['br_id'];
-
-$result = mysql_query("Select * from department_master where br_id = '$BR_ID'") or die("Error");
+$result = mysql_query("Select DISTINCT * from bed_master") or die("Error");
 
 if (mysql_num_rows($result) > 0) {
     // looping through all results
     // products node
-    $response["department"] = array();
+    $response["bednumber"] = array();
     
     while ($row = mysql_fetch_array($result)) {
         // temp user array
-        $Department = array();
-        $Department["BR_ID"] = $row["BR_ID"];
-        $Department["dept_name"] = $row["dept_name"];
+        $Bednumber = array();
+        $Bednumber["bed_id"] = $row["bed_id"];
+        $Bednumber["bed_no"] = $row["bed_no"];
         //$services["SERV_PRICE"] = $row["SERV_PRICE"];
         // push single product into final response array
-        array_push($response["department"], $Department);
+        array_push($response["bednumber"], $Bednumber);
     }
     // success
     $response["success"] = 1;
@@ -30,16 +25,10 @@ if (mysql_num_rows($result) > 0) {
     echo json_encode($response);
 } else {
     // no products found
-    $response["success"] = 2;
+    $response["success"] = 0;
     $response["message"] = "No data found";
 
     // echo no users JSON
-    echo json_encode($response);
-}
-}
-else {
-    $response["missing"] = 3;
-    $response["error_msg"] = "Required parameters is missing!";
     echo json_encode($response);
 }
 ?>
--- webserv/pharmacyall.php
+++ webserv/pharmacyall.php
@@ -2,25 +2,21 @@
 include 'include/Config.php';
 $db = new DB_Class();
 
-if (isset($_POST['sp_id'])) {
-
-    $SP_ID = $_POST['sp_id'];
-
-$result = mysql_query("Select * from surgery_type_master where sp_id = '$SP_ID'") or die("Error");
+$result = mysql_query("Select DISTINCT * from pharmacy_master") or die("Error");
 
 if (mysql_num_rows($result) > 0) {
     // looping through all results
     // products node
-    $response["surgerytype"] = array();
+    $response["pharmacy"] = array();
     
     while ($row = mysql_fetch_array($result)) {
         // temp user array
-        $Surgerytype = array();
-        $Surgerytype["sp_id"] = $row["sp_id"];
-        $Surgerytype["surgery_type"] = $row["surgery_type"];
+        $Pharmacy = array();
+        $Pharmacy["pharma_id"] = $row["pharma_id"];
+        $Pharmacy["pharmacy_name"] = $row["pharmacy_name"];
         //$services["SERV_PRICE"] = $row["SERV_PRICE"];
         // push single product into final response array
-        array_push($response["surgerytype"], $Surgerytype);
+        array_push($response["pharmacy"], $Pharmacy);
     }
     // success
     $response["success"] = 1;
@@ -29,16 +25,10 @@ if (mysql_num_rows($result) > 0) {
     echo json_encode($response);
 } else {
     // no products found
-    $response["success"] = 2;
+    $response["success"] = 0;
     $response["message"] = "No data found";
 
     // echo no users JSON
-    echo json_encode($response);
-}
-}
-else {
-    $response["missing"] = 3;
-    $response["error_msg"] = "Required parameters is missing!";
     echo json_encode($response);
 }
 ?>
